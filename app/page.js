3a11cd824188165f6e5187cdce8f4779ac9336e5
Anvit Verma <anvitverma1199@gmail.com>
2026-01-29 22:14:58 +0530

    updated footer

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ import Jobs from "../src/components/sections/Jobs";
 import SelectedWorks from "../src/components/sections/SelectedWorks";
 import Education from "../src/components/sections/Education"; 
 import Skills from "../src/components/sections/Skills";
-import MorphingFooter from "../src/components/sections/MorphingFooter"; // <--- Import This
+import MorphingFooter from "../src/components/sections/MorphingFooter"; 
 
 export default function Home() {
   return (
     <main className="px-6 md:px-24 lg:px-36 min-h-screen w-full overflow-x-hidden">
       <div className="max-w-[1000px] mx-auto w-full">
         
+        {/* Main Sections */}
         <Hero />
         <About />
         <Jobs />
@@ -18,15 +19,12 @@ export default function Home() {
         <Education />
         <Skills />
         
-        {/* The Morphing Footer */}
+
+        
+        {/* 3. Morphing Footer at the very bottom */}
         <MorphingFooter />
         
-        {/* Simple Copyright Footer */}
-        <section className="pb-10 flex items-center justify-center text-center">
-            <p className="font-mono text-sm text-[#8892b0]">
-              Built & Designed by Anvit Verma
-            </p>
-        </section>
+        {/* 4. Removed the "Built & Designed by" section completely */}
       </div>
     </main>
   );
