a19caff8a54749e6093a940cf9e42c80abe2804b
Anvit Verma <anvitverma1199@gmail.com>
2026-06-23 17:49:52 +0530

    Revert intro animation completely

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Hero from "../src/components/sections/Hero";
 import Jobs from "../src/components/sections/Jobs";
 import Publications from "../src/components/sections/Publications"; 
@@ -8,48 +8,23 @@ import Education from "../src/components/sections/Education";
 import Skills from "../src/components/sections/Skills";
 import MorphingFooter from "../src/components/sections/MorphingFooter"; 
 import RotatingStar from "../src/components/RotatingStar"; 
-import IntroAnimation from "../src/components/IntroAnimation";
 
 export default function Home() {
-  const [showIntro, setShowIntro] = useState(true);
-
-  // Lock scrolling while intro is playing
-  useEffect(() => {
-    if (showIntro) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-  }, [showIntro]);
-
   return (
     <main className="min-h-screen w-full relative">
-      
-      {showIntro && (
-        <IntroAnimation onComplete={() => setShowIntro(false)} />
-      )}
+      <RotatingStar />
 
-      {/* Main content fades in as intro finishes */}
-      <div 
-        className={`transition-opacity duration-1000 ease-in-out ${
-          showIntro ? "opacity-0" : "opacity-100"
-        }`}
-      >
-        <RotatingStar />
-
-        <div className="px-6 md:px-24 lg:px-36 w-full relative z-10">
-          <div className="max-w-[800px] mx-auto w-full">
-            <Hero />
-            <Jobs />
-            <Publications />
-            <SelectedWorks />
-            <Education />
-            <Skills />
-            <MorphingFooter />
-          </div>
+      <div className="px-6 md:px-24 lg:px-36 w-full relative z-10">
+        <div className="max-w-[800px] mx-auto w-full">
+          <Hero />
+          <Jobs />
+          <Publications />
+          <SelectedWorks />
+          <Education />
+          <Skills />
+          <MorphingFooter />
         </div>
       </div>
-      
     </main>
   );
 }
